e7d0b4a0764e3e7121b16e496520ea52c2ab5741
knezl <knezl@result.si>
2024-04-10 23:24:38 +0200

    ure component

---
+++ projects/example/src/ure/ure.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { UreComponent } from './ure.component';
+
+describe('UreComponent', () => {
+  let component: UreComponent;
+  let fixture: ComponentFixture<UreComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [UreComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(UreComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ projects/example/src/ure/ure.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ure',
+  standalone: true,
+  imports: [],
+  templateUrl: './ure.component.html',
+  styleUrl: './ure.component.css'
+})
+export class UreComponent {
+
+}
